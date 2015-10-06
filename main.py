9ed3e14b7720a26bd3c8530f3da8f68eeb198733
dth26 <dth26@pitt.edu>
2015-10-06 00:42:27 -0400

    create base.css
    move menu css from index to base.css

--- main.py
+++ main.py
@@ -11,12 +11,14 @@ import os.path
 import controllers.indexController as indexController
 import controllers.galleryController as galleryController
 import controllers.profileController as profileController
+import controllers.testGalleryController as testGalleryController
 
 # set up paths
 mappings = [
     ('/', indexController.index),
     ('/gallery', galleryController.index),
-    ('/profile', profileController.index)
+    ('/profile', profileController.index),
+    ('/testGallery', testGalleryController.index)
 ]
 
 
