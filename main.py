d5a910a0423dca734870767b6c5c3f7c5d7ec51c
dth26 <dth26@pitt.edu>
2015-10-06 11:40:03 -0400

    fix paths

--- main.py
+++ main.py
@@ -11,22 +11,16 @@ import os.path
 import controllers.indexController as indexController
 import controllers.galleryController as galleryController
 import controllers.profileController as profileController
-<<<<<<< HEAD
-=======
+
 import controllers.testGalleryController as testGalleryController
->>>>>>> f4490f0644c17459d5dbb68ef9e55ea4b6aac121
+
 
 # set up paths
 mappings = [
     ('/', indexController.index),
     ('/gallery', galleryController.index),
-<<<<<<< HEAD
-    # ('/catagory', catagoryController.index),
-    ('/profile', profileController.index)
-=======
     ('/profile', profileController.index),
     ('/testGallery', testGalleryController.index)
->>>>>>> f4490f0644c17459d5dbb68ef9e55ea4b6aac121
 ]
 
 
