be78f0359d31518bccd2134522540d6a2ab22ff7
Charlotte Chen <charlottechen21@gmail.com>
2015-10-06 20:06:21 -0400

    Updated homepage with better filler photo

--- controllers/indexController.py
+++ controllers/indexController.py
@@ -16,10 +16,17 @@ class index(webapp2.RequestHandler):
     def get(self):
         # images = imagesModels.getImages()
         app_global.render_template(self,'index.html',{'page_name':'Thrifty Clothes', 
-                                                      'photo_name':'test.jpg', 
-                                                      'cow1':'Baby-Cow.jpg', 
-                                                      'cow2':'Double-Cow.jpg', 
-                                                      'cow3':'Fluffy-Cow.jpg', 
-                                                      'cow4':'Orange-Cow.jpg'})
+                                                      'photo_name':'male-photo.jpg', 
+                                                      'photo_name2':'test.jpg',
+                                                      'photo_name3':'sketch.jpg',
+                                                      'cow1':'girl1.jpg', 
+                                                      'cow2':'girl2.jpg', 
+                                                      'cow3':'girl3.jpg', 
+                                                      'cow4':'girl4.jpg',
+                                                      'girl5':'girl9.jpg', 
+                                                      'girl6':'girl10.jpg', 
+                                                      'girl7':'girl11.jpg', 
+                                                      'girl8':'girl12.jpg',
+                                                     })
 		
 		
