af54b25e085d11e8ecf6fda44967f37fc0e07c2d
kaskales <kaskalesuisse@gmail.com>
2015-10-30 13:56:44 -0400

    update profile controllers

--- controllers/profileController.py
+++ controllers/profileController.py
@@ -1,5 +1,8 @@
 import webapp2
-
+import models.userModel as userModel
+import app_global
+from webapp2_extras import sessions
+import indexController
 # import path to models
 import sys
 sys.path.insert(0, '/models')
@@ -10,10 +13,17 @@ import main
     
     
 # When webapp2 receives an HTTP GET request to the URL /, it instantiates the index class
-class index(webapp2.RequestHandler):
+class index(indexController.index):
+        
     #respond to HTTP GET requests
     def get(self):
-        # images = imagesModels.getImages()
-        app_global.render_template(self,'profile.html', {'username':'Username',                                                                             'profilePic':'profilePic.jpeg'})
+        user = self.session.get('user')
+        
+        params = {
+            'user': user,
+            'user_id': self.session.get('user_id')
+        }
+        
+        app_global.render_template(self, 'profile.html', params)
 		  
 		
