ce7a3c37a1a048029da59f5205520236098cfc79
dth26 <dth26@pitt.edu>
2015-10-31 00:10:48 -0400

    remove import requests

--- models/imagesModel.py
+++ models/imagesModel.py
@@ -5,7 +5,7 @@ from google.appengine.api import users
 from google.appengine.ext import blobstore
 from google.appengine.ext.webapp import blobstore_handlers
 import json
-import requests
+#import requests
 
 #
 #def get_user_email():
