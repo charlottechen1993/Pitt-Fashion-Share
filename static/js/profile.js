6b1096ebd6c829f52c10725f6e06f78d52c68a09
Charlotte Chen <charlottechen21@gmail.com>
2015-12-07 18:39:06 -0500

    Fixed filter, added more tag options

--- static/js/profile.js
+++ static/js/profile.js
@@ -1,12 +1,12 @@
  angularAPP.controller('profileController', function($scope,$http){
      $scope.categories = [];
      $scope.loadOption = function(){
-          $scope.categories.push('Chic', 'Finals', 'Parisian', 'Lazy Day', 'Pitt Spirit', 'Cool', 'Elegant', 'Tomboy', 'Metal', 'Bro', 'Halloween', 'Goofy', 'Comfortable', 'High Fashion', 'British', 'Conservative', 'Sexy');
+          $scope.categories.push('Chic', 'Finals', 'Parisian', 'Lazy Day', 'Pitt Spirit', 'Cool', 'Elegant', 'Tomboy', 'Metal', 'Bro', 'Halloween', 'Goofy', 'Comfortable', 'High Fashion', 'British', 'Conservative', 'Sexy', 'Exotic', 'Athletic', 'Black & White', 'Cute', 'Casual', 'Vintage', 'Winter', 'Spring', 'Summer', 'Fall', 'Prom', 'Special Occasion', 'Church', 'Warm');
      }
      
      $scope.categories = [];
      $scope.loadOption = function(){
-          $scope.categories.push('Chic', 'Finals', 'Parisian', 'Lazy Day', 'Pitt Spirit', 'Cool', 'Elegant', 'Tomboy', 'Metal', 'Bro', 'Halloween', 'Goofy', 'Comfortable', 'High Fashion', 'British', 'Conservative', 'Sexy');
+          $scope.categories.push('Chic', 'Finals', 'Parisian', 'Lazy Day', 'Pitt Spirit', 'Cool', 'Elegant', 'Tomboy', 'Metal', 'Bro', 'Halloween', 'Goofy', 'Comfortable', 'High Fashion', 'British', 'Conservative', 'Sexy', 'Exotic', 'Athletic', 'Black & White', 'Cute', 'Casual', 'Vintage', 'Winter', 'Spring', 'Summer', 'Fall', 'Prom', 'Special Occasion', 'Church', 'Warm');
      }
      
      $scope.selected = [];
